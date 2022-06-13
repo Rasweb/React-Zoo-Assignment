e8360355c9e30922d15b1854a636978e7172f40d
Rasweb <Rasmuspalm@fastmail.se>
2022-06-13 16:37:52 +0200

    Added time code and specific id localStorage to done

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -18,39 +18,104 @@ export const Animal = () => {
   });
   const [fedAnimal, setFedAnimal] = useState(false);
   const [fedTime, setFedTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(new Date());
   const [hasBeenFed, setHasBeenFed] = useState(false);
 
   let params = useParams();
 
   useEffect(() => {
     // Checks if animal exist
-    if (!animal) return;
+    // if (!animal) return;
     // if animal dosen't exist
     // Fetch data with axios
-    axios
-      .get<IMoreAnimal>(
-        "https://animals.azurewebsites.net/api/animals/" + params.id
-      )
-      // If response
-      .then((data) => {
-        // Change value of setAnimal to data.data
-        setAnimal(data.data);
-      })
-      // If error
-      .catch((error) => {
-        // Console.log the error
-        console.log("bad", error);
-      });
-  });
+    // axios
+    //   .get<IMoreAnimal>(
+    //     "https://animals.azurewebsites.net/api/animals/" + params.id
+    //   )
+    //   // If response
+    //   .then((data) => {
+    //     // Change value of setAnimal to data.data
+    //     setAnimal(data.data);
+    //   })
+    //   // If error
+    //   .catch((error) => {
+    //     // Console.log the error
+    //     console.log("bad", error);
+    //   });
+
+    const specificAnimal = localStorage.getItem("animals");
+    if (specificAnimal) {
+      const parsedSpecific = JSON.parse(specificAnimal);
+
+      for (let i = 0; i < parsedSpecific.length; i++) {
+        if (parsedSpecific[i].id === params.id) {
+          setAnimal(parsedSpecific);
+          console.log(parsedSpecific[i]);
+        } else {
+          console.log("Bad");
+          console.log(params.id, "Params");
+          console.log(animal.id, "Specific ID");
+        }
+      }
+    }
+  }, []);
 
   useEffect(() => {
     // Check time every 1 second.
+    const currentDate = setInterval(() => setCurrentTime(new Date()), 10000);
     let timer = setInterval(() => setFedTime(new Date()), 1000);
     // To restart the time process and avoid memory leaks
     return function cleanup() {
       clearInterval(timer);
+      clearInterval(currentDate);
     };
   });
+
+  useEffect(() => {
+    const animalTime = localStorage.getItem("animals");
+    if (animalTime) {
+      const parseAnimalTime = JSON.parse(animalTime);
+      console.log(parseAnimalTime);
+
+      for (let i = 0; i < parseAnimalTime.length; i++) {
+        if (parseAnimalTime[i].lastFed === animal.lastFed)
+          console.log(`${parseAnimalTime[i].lastFed} This is lastFed`);
+        console.log(`${currentTime.toLocaleTimeString()} This is currentTime`);
+
+        let timeLeft = Date.parse(parseAnimalTime[i].lastFed) - Date.now();
+        let timeLef =
+          Date.parse(parseAnimalTime[i].lastFed) -
+          Date.parse(currentTime.toLocaleTimeString());
+        console.log(fedTime);
+
+        console.log(timeLeft, "hes");
+        console.log(timeLef);
+      }
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   const animalStored = localStorage.getItem("animals");
+  //   // If animals exist
+  //   if (animalStored) {
+  //     // Parse animals from JSON
+  //     const parsedAnimal = JSON.parse(animalStored);
+  //     // Loop through animals
+  //     for (let i = 0; i < parsedAnimal.length; i++) {
+  //       // If current animals.id === animal.id
+  //       if (parsedAnimal[i].isFed === true) {
+  //         // Change boolean to true.
+  //         setFedAnimal(true);
+  //         setHasBeenFed(true);
+  //       } else {
+  //         setFedAnimal(false);
+  //         setHasBeenFed(false);
+  //       }
+  //     }
+  //     console.log(parsedAnimal);
+  //     console.log(fedTime.toLocaleTimeString());
+  //   }
+  // });
 
   const feedAnimal = () => {
     // Get animals from localStorage
@@ -66,14 +131,13 @@ export const Animal = () => {
           // Change boolean to true.
           parsedAnimal[i].isFed = !animal.isFed;
           parsedAnimal[i].lastFed = fedTime.toLocaleTimeString();
+          localStorage.setItem("animals", JSON.stringify(parsedAnimal));
+          setFedAnimal(true);
+          setHasBeenFed(true);
         }
       }
       console.log(parsedAnimal);
       console.log(fedTime.toLocaleTimeString());
-
-      localStorage.setItem("animals", JSON.stringify(parsedAnimal));
-      setFedAnimal(true);
-      setHasBeenFed(true);
     }
   };
 
